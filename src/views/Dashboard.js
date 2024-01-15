2c4aed35eac79d0736ae6c80998716869a6fd1b0
Gianvar21 <107148012+LFloresMgsa@users.noreply.github.com>
2024-01-15 10:51:24 -0500

    p

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -24,7 +24,7 @@ const WhiteBackground = styled.div`
 const FooterRoot = muiStyled('footer')(
   ({ theme }) => css`
   position: absolute;
-  bottom: 0;
+  bottom: a0;
   left: 50%;
   transform: translateX(-35%);
   text-align: center;
